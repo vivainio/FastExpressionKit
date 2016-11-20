5edf27ebcfd69b00dc97f521a8244736a97319af
Ville M. Vainio <ville.vainio@basware.com>
2016-11-20 15:49:06 +0200

    move files

---
+++ FastExpressionKit/FastObjectDiffer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace FastExpressionKit
+{
+    public static class EE
+    {
+        public static ParameterExpression Var<T>(string name) => Expression.Variable(typeof(T), name);
+        public static ParameterExpression Param<T>(string name) => Expression.Parameter(typeof(T), name);
+        public static MemberExpression Dot(this Expression exp, string fieldName) => Expression.PropertyOrField(exp, fieldName);
+        public static Expression IsEq(this Expression left, Expression right) => Expression.Equal(left, right);
+    }
+
+    public class FastObjectDiffer<T1, T2>
+    {
+        public readonly String[] Props;
+        private readonly Func<T1, T2, bool[]> comparisonFunc;
+        private Func<T1, T2, bool[]> CreateExpression(IEnumerable<string> fields)
+        {
+            var t1param = EE.Param<T1>("left");
+            var t2param = EE.Param<T2>("right");
+
+            var cmplist = fields.Select(f => t1param.Dot(f).IsEq(t2param.Dot(f)));
+            var resultArr = Expression.NewArrayInit(typeof(bool), cmplist);
+            var l = Expression.Lambda<Func<T1, T2, bool[]>>(resultArr, t1param, t2param);
+            return l.Compile();
+        }
+        public FastObjectDiffer(string[] props)
+        {
+            this.Props = props;
+            this.comparisonFunc = CreateExpression(props);
+        }
+
+        public bool[] Compare(T1 left, T2 right) => comparisonFunc.Invoke(left, right);
+    }
+}
--- FastExpressionKit/Program.cs
+++ FastExpressionKit/Program.cs
@@ -1,15 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FastExpressionKit;
+using System.Linq.Expressions;
 
-namespace FastExpressionKit
+namespace ConsoleApplication2
 {
+    public class C
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+    }
+    public class D
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public int c { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            // demo data
+            var c1 = new C() { a = 666, b = 12 };
+            var c2 = new C() { a = 100, b = 12 };
+            var d1 = new D() { a = 666, b = 12, c = 123 };
+            var d2 = new D() { a = 100, b = 12 , c = 223 };
+
+            var differ = new FastObjectDiffer<C, C>(new[] { "a", "b" });
+            var res = differ.Compare(c1, c2);
+            // [ false, true]
+
+            // compare different types!
+            var differ2 = new FastObjectDiffer<C, D>(new[] { "a", "b" });
+            res = differ2.Compare(c1, d1);
         }
+            
     }
 }
