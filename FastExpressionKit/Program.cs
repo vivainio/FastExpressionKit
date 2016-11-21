9d5a024d5e6d958f139a3047e0c4626fbb83bf8a
Ville M. Vainio <ville.vainio@basware.com>
2016-11-21 21:45:18 +0200

    tests to nunit

--- FastExpressionKit/Program.cs
+++ FastExpressionKit/Program.cs
@@ -1,218 +1,41 @@
-﻿using FakePoc;
-using FastExpressionKit;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Linq.Expressions;
-using static System.Console;
+// ***********************************************************************
+// Copyright (c) 2015 Charlie Poole
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ***********************************************************************
 
-namespace ConsoleApplication2
+using NUnitLite;
+
+namespace NUnitLite.Tests
 {
-    public class C
+    public class Program
     {
-        public int a { get; set; }
-        public int b { get; set; }
-        public DateTime date { get; set; }
-        public DateTime? mynullable { get; set; }
-    }
-    public class D
-    {
-        public int a { get; set; }
-        public int b { get; set; }
-        public int c { get; set; }
-        public DateTime date { get; set; }
-        public DateTime? mynullable { get; set; }
-
-    }
-
-    class Program
-    {
-        static void RepeatBench(string description, int n, Action action)
+        /// <summary>
+        /// The main program executes the tests. Output may be routed to
+        /// various locations, depending on the arguments passed.
+        /// </summary>
+        /// <remarks>Run with --help for a full list of arguments supported</remarks>
+        /// <param name="args"></param>
+        public static int Main(string[] args)
         {
-            var sw = Stopwatch.StartNew();
-            for (var i = 0; i < n; i++)
-            {
-                action();
-            }
-            WriteLine("{0}: {1}", description, (sw.ElapsedMilliseconds * 1000) / (float) n);
-
+            return new AutoRun().Execute(args);
         }
-        static void Benchmark()
-        {
-            var c1 = new C() { a = 666, b = 12, date = DateTime.Now, mynullable = DateTime.Now };
-            var d1 = new D() { a = 666, b = 8, c = 9, date = DateTime.Now, mynullable = DateTime.Now };
-
-            var fields = new[] { "a", "b" };
-
-
-            WriteLine("Stats (microseconds) per iteration");
-
-            RepeatBench("new Differ()", 1000, () =>
-            {
-                var dd = new Differ<C, D>(fields);
-            });
-
-            var d = new Differ<C, D>(fields);
-
-            RepeatBench("Compare small objects", 1000000, () =>
-            {
-                var res = d.Compare(c1, d1);
-            });
-            RepeatBench("new FieldExtract()", 1000, () =>
-            {
-                var dd = new FieldExtract<C, int>(fields);
-            });
-            var extractor = new FieldExtract<C, int>(fields);
-
-            RepeatBench("Extract integers", 1000000, () =>
-            {
-                var ints = extractor.Extract(c1);
-            });
-
-            // big data
-            var big1 = new BigDto();
-            var big2 = new BigDto();
-
-            var bigprops = ReflectionHelper.CollectProps<BigDto>();
-            var bigpropnames = bigprops.SelectMany(p => p.Item2).ToArray();
-
-            RepeatBench("new Differ() for big class", 100, () =>
-            {
-                var dd = new Differ<BigDto, BigDto>(bigpropnames);
-
-            });
-
-            var bigd = new Differ<BigDto, BigDto>(bigpropnames);
-            RepeatBench("Compare large objects", 10000, () =>
-            {
-                bigd.Compare(big1, big2);
-            });
-
-            var types = ReflectionHelper.CollectProps<BigDto>();
-            var e4 = ReflectionHelper.GetExtractorFor<BigDto, int>(types);
-            var e5 = ReflectionHelper.GetExtractorFor<BigDto, string>(types);
-            var e6 = ReflectionHelper.GetExtractorFor<BigDto, decimal>(types);
-
-           
-            RepeatBench("Extract fields from large object", 10000, () =>
-            {
-                var r1 = e4.Extract(big1);
-                var r2 = e5.Extract(big1);
-                var r3 = e6.Extract(big1);
-            });
-
-
-            RepeatBench("Extract fields from large object, convert to dict, naive", 10000, () =>
-            {
-                var pd = e4.ResultsAsDict(e4.Extract(big1).Select(i => i.ToString()).ToList())
-                       .Union(e5.ResultsAsDict(e5.Extract(big1).Select(e => e.ToString()).ToList()))
-                       .Union(e6.ResultsAsDict(e6.Extract(big1).Select(e => e.ToString()).ToList()));
-            });
-
-            var boxedExtract = new FieldExtract<BigDto, object>(bigpropnames);
-
-            RepeatBench("Extract fields, boxed", 100000, () =>
-            {
-                var r1 = boxedExtract.Extract(big1);
-            });
-
-            RepeatBench("Extract fields from large dto. string -> object dict", 10000, () =>
-            {
-                var r1 = boxedExtract.Extract(big1);
-                var r2 = boxedExtract.ResultsAsZip(r1);
-            });
-
-
-            var propertyInfos = typeof(BigDto).GetProperties();
-            RepeatBench("Extract fields with reflection", 10000, () =>
-            {
-                foreach (var p in propertyInfos)
-                {
-                    var val = p.GetValue(big1);
-                }
-            });
-
-            RepeatBench("Extract fields with reflection, convert to string dict", 10000, () =>
-            {
-                var resdict = new Dictionary<string, string>();
-                foreach (var p in propertyInfos)
-                {
-                    var val = p.GetValue(big1);
-                    var s = val.ToString();
-                    resdict[p.Name] = s;
-                }
-            });
-
-
-            var copier = new FieldCopier<BigDto, BigDto>(bigpropnames);
-            RepeatBench("Copy big object", 10000, () =>
-            {
-                copier.Copy(big1, big2);
-            });
-
-            ReadLine();
-        }
-        static void Main(string[] args)
-        {
-            // demo data
-            var c1 = new C() { a = 666, b = 12, date = DateTime.Now, mynullable = DateTime.Now };
-            var c2 = new C() { a = 100, b = 12, mynullable = null };
-            var d1 = new D() { a = 666, b = 12, c = 123 };
-            var d2 = new D() { a = 100, b = 12 , c = 223 };
-            var fields = new[] { "a", "b" };
-
-            var differ = new Differ<C, C>(new[] { "a", "b" });
-            var res = differ.Compare(c1, c2);
-            // [ false, true]
-
-            // compare different types!
-            var differ2 = new Differ<C, D>(new[] { "a", "b" });
-            res = differ2.Compare(c1, d1);
-
-            var extractor = new FieldExtract<C, int>(fields);
-            var results = extractor.Extract(c1);
-            //var dresults = extractor.ExtractAsDict(c1);
-            try
-            {
-                var fails = new FieldExtract<C, string>(fields);
-            } catch (InvalidOperationException e) {};
-
-            var ee = new FieldExtract<C, DateTime?>(new[] { "mynullable" });
-            var r = ee.Extract(c1);
-
-            //var e2 = new FieldExtract<C, object>(fields);
-            //e2.Extract(c1);
-
-            var e3 = new FieldExtract<C, DateTime>(new[] { "date" });
-            var r3 = e3.Extract(c1);
-
-            // automatically create extractors per each property type
-            var types = ReflectionHelper.CollectProps<C>();
-            var e4 = ReflectionHelper.GetExtractorFor<C, int>(types);
-            var e5 = ReflectionHelper.GetExtractorFor<C, DateTime>(types);
-            var e6 = ReflectionHelper.GetExtractorFor<C, DateTime?>(types);
-
-
-
-            Action<C> tryit = c =>
-            {
-                // smash exctractor results together with conversions to get string
-
-                var pd = e4.ResultsAsDict(e4.Extract(c).Select(i => i.ToString()).ToList())
-                    .Union(e5.ResultsAsDict(e5.Extract(c).Select(e => e.ToString()).ToList()))
-                    .Union(e6.ResultsAsDict(e6.Extract(c).Select(e => e.ToString()).ToList()));
-            };
-
-            tryit(c1);
-            tryit(c2);
-            c1.a = 111;
-            c1.b = 999;
-            var copier = new FieldCopier<C, C>(fields);
-            copier.Copy(c1, c2);
-
-            Benchmark();
-        }
-
     }
 }
---
+++ FastExpressionKit/Test.cs
@@ -0,0 +1,238 @@
+﻿using FakePoc;
+using FastExpressionKit;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using static System.Console;
+
+namespace FastExpressionKitTests
+{
+    public class C
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public DateTime date { get; set; }
+        public DateTime? mynullable { get; set; }
+    }
+    public class D
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public int c { get; set; }
+        public DateTime date { get; set; }
+        public DateTime? mynullable { get; set; }
+
+    }
+    [TestFixture]
+
+    class FastExprKitTest
+    {
+        static void RepeatBench(string description, int n, Action action)
+        {
+            var sw = Stopwatch.StartNew();
+            for (var i = 0; i < n; i++)
+            {
+                action();
+            }
+            WriteLine("{0}: {1}", description, (sw.ElapsedMilliseconds * 1000) / (float) n);
+
+        }
+        [Test]
+        public static void Benchmark()
+        {
+            var c1 = new C() { a = 666, b = 12, date = DateTime.Now, mynullable = DateTime.Now };
+            var d1 = new D() { a = 666, b = 8, c = 9, date = DateTime.Now, mynullable = DateTime.Now };
+
+            var fields = new[] { "a", "b" };
+
+
+            WriteLine("Stats (microseconds) per iteration");
+
+            RepeatBench("new Differ()", 1000, () =>
+            {
+                var dd = new Differ<C, D>(fields);
+            });
+
+            var d = new Differ<C, D>(fields);
+
+            RepeatBench("Compare small objects", 1000000, () =>
+            {
+                var res = d.Compare(c1, d1);
+            });
+            RepeatBench("new FieldExtract()", 1000, () =>
+            {
+                var dd = new FieldExtract<C, int>(fields);
+            });
+            var extractor = new FieldExtract<C, int>(fields);
+
+            RepeatBench("Extract integers", 1000000, () =>
+            {
+                var ints = extractor.Extract(c1);
+            });
+
+            // big data
+            var big1 = new BigDto();
+            var big2 = new BigDto();
+
+            var bigprops = ReflectionHelper.CollectProps<BigDto>();
+            var bigpropnames = bigprops.SelectMany(p => p.Item2).ToArray();
+
+            RepeatBench("new Differ() for big class", 100, () =>
+            {
+                var dd = new Differ<BigDto, BigDto>(bigpropnames);
+
+            });
+
+            var bigd = new Differ<BigDto, BigDto>(bigpropnames);
+            RepeatBench("Compare large objects", 10000, () =>
+            {
+                bigd.Compare(big1, big2);
+            });
+
+            var types = ReflectionHelper.CollectProps<BigDto>();
+            var e4 = ReflectionHelper.GetExtractorFor<BigDto, int>(types);
+            var e5 = ReflectionHelper.GetExtractorFor<BigDto, string>(types);
+            var e6 = ReflectionHelper.GetExtractorFor<BigDto, decimal>(types);
+
+           
+            RepeatBench("Extract fields from large object", 10000, () =>
+            {
+                var r1 = e4.Extract(big1);
+                var r2 = e5.Extract(big1);
+                var r3 = e6.Extract(big1);
+            });
+
+
+            RepeatBench("Extract fields from large object, convert to dict, naive", 10000, () =>
+            {
+                var pd = e4.ResultsAsDict(e4.Extract(big1).Select(i => i.ToString()).ToList())
+                       .Union(e5.ResultsAsDict(e5.Extract(big1).Select(e => e.ToString()).ToList()))
+                       .Union(e6.ResultsAsDict(e6.Extract(big1).Select(e => e.ToString()).ToList()));
+            });
+
+            var boxedExtract = new FieldExtract<BigDto, object>(bigpropnames);
+
+            RepeatBench("Extract fields, boxed", 100000, () =>
+            {
+                var r1 = boxedExtract.Extract(big1);
+            });
+
+            RepeatBench("Extract fields from large dto. string -> object dict", 10000, () =>
+            {
+                var r1 = boxedExtract.Extract(big1);
+                var r2 = boxedExtract.ResultsAsZip(r1);
+            });
+
+
+            var propertyInfos = typeof(BigDto).GetProperties();
+            RepeatBench("Extract fields with reflection", 10000, () =>
+            {
+                foreach (var p in propertyInfos)
+                {
+                    var val = p.GetValue(big1);
+                }
+            });
+
+            RepeatBench("Extract fields with reflection, convert to string dict", 10000, () =>
+            {
+                var resdict = new Dictionary<string, string>();
+                foreach (var p in propertyInfos)
+                {
+                    var val = p.GetValue(big1);
+                    var s = val.ToString();
+                    resdict[p.Name] = s;
+                }
+            });
+
+
+            var copier = new FieldCopier<BigDto, BigDto>(bigpropnames);
+            RepeatBench("Copy big object", 100000, () =>
+            {
+                copier.Copy(big1, big2);
+            });
+
+        }
+
+        // test data
+        static C c1 = new C() { a = 666, b = 12, date = DateTime.Now, mynullable = DateTime.Now };
+        static C c2 = new C() { a = 100, b = 12, mynullable = null };
+        static D d1 = new D() { a = 666, b = 12, c = 123 };
+        static D d2 = new D() { a = 100, b = 12, c = 223 };
+        static string[] fields = new[] { "a", "b" };
+
+        [Test]
+        public static void StartTests()
+        {
+            // demo data
+
+            TestCopier();
+
+            //Benchmark();
+        }
+        [Test]
+        public static void TestCopier()
+        {
+            c1.a = 111;
+            c1.b = 999;
+            var copier = new FieldCopier<C, C>(fields);
+            copier.Copy(c1, c2);
+        }
+
+        public static void ManyExtractors()
+        {
+            // automatically create extractors per each property type
+            var types = ReflectionHelper.CollectProps<C>();
+            var e4 = ReflectionHelper.GetExtractorFor<C, int>(types);
+            var e5 = ReflectionHelper.GetExtractorFor<C, DateTime>(types);
+            var e6 = ReflectionHelper.GetExtractorFor<C, DateTime?>(types);
+
+            Action<C> tryit = c =>
+            {
+                // smash exctractor results together with conversions to get string
+
+                var pd = e4.ResultsAsDict(e4.Extract(c).Select(i => i.ToString()).ToList())
+                    .Union(e5.ResultsAsDict(e5.Extract(c).Select(e => e.ToString()).ToList()))
+                    .Union(e6.ResultsAsDict(e6.Extract(c).Select(e => e.ToString()).ToList()));
+            };
+
+            tryit(c1);
+            tryit(c2);
+        }
+
+        public static void FieldExtractTest()
+        {
+            var extractor = new FieldExtract<C, int>(fields);
+            var results = extractor.Extract(c1);
+            //var dresults = extractor.ExtractAsDict(c1);
+            try
+            {
+                var fails = new FieldExtract<C, string>(fields);
+            }
+            catch (InvalidOperationException e) { };
+
+            var ee = new FieldExtract<C, DateTime?>(new[] { "mynullable" });
+            var r = ee.Extract(c1);
+
+            //var e2 = new FieldExtract<C, object>(fields);
+            //e2.Extract(c1);
+
+            var e3 = new FieldExtract<C, DateTime>(new[] { "date" });
+            var r3 = e3.Extract(c1);
+        }
+
+        [Test]
+        public static void DifferSmall()
+        {
+            var differ = new Differ<C, C>(new[] { "a", "b" });
+            var res = differ.Compare(c1, c2);
+            // [ false, true]
+
+            // compare different types!
+            var differ2 = new Differ<C, D>(new[] { "a", "b" });
+            res = differ2.Compare(c1, d1);
+        }
+    }
+}
