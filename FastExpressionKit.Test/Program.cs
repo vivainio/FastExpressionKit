3dac0f3e1755be605450c1cb979eb20747979ccf
Ville M. Vainio <ville.vainio@basware.com>
2018-04-27 18:44:02 +0300

    switch to netstandard + sdk build

---
+++ FastExpressionKit.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastExpressionKit.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
