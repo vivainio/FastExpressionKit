50028342af7e7007cd5b6f9d3816e5f98e660d3c
Ville M. Vainio <ville.vainio@basware.com>
2018-04-29 15:58:10 +0300

    packaging

---
+++ publish.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+
+import os,shutil
+
+prjdir = "FastExpressionKit"
+version = "1.0.0.0"
+def c(s):
+    print(">",s)
+    err = os.system(s)
+    assert not err
+
+def nuke(pth):
+    if os.path.isdir(pth):
+        shutil.rmtree(pth)
+
+nuke(prjdir + "/bin")
+nuke(prjdir + "/obj")
+
+os.chdir("%s.Test" % prjdir )
+c("dotnet run")
+
+def pack():
+    c("dotnet pack /p:Version=%s" % version)
+
+
+os.chdir("../" + prjdir)
+pack()
