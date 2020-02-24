ed2b63f75a7f8c63f1a15a39c6d50c209498ad20
Ville M. Vainio <ville.vainio@basware.com>
2020-02-24 21:13:41 +0200

    integration tests

--- FastExpressionKit.Integration.Tests/Program.cs
+++ FastExpressionKit.Integration.Tests/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TrivialTestRunner;
 
 namespace FastExpressionKit.Integration.Tests
 {
@@ -6,7 +7,9 @@ namespace FastExpressionKit.Integration.Tests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TRunner.AddTests<IntegrationTests>();
+            TRunner.RunTests();
+            TRunner.ReportAll();
         }
     }
 }
