34574a14bbdc1780b527a668873695c985560f3f
Ville M. Vainio <ville.vainio@basware.com>
2020-02-24 19:45:30 +0200

    integration tests

---
+++ FastExpressionKit.Integration.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastExpressionKit.Integration.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
