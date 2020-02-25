db7c4c228aa1fcaeecc64f5759a7b10a21578901
Ville M. Vainio <ville.vainio@basware.com>
2020-02-25 12:28:53 +0200

    testentities

---
+++ FastExpressionKit.Integration.Tests/TestIntities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastExpressionKit.Integration.Tests
+{
+    [Table("DB_VERSION_INFO")]
+    class TestDbEntity
+    {
+        [Column("VERSION")]
+        public Guid MyId { get; set; }
+
+        [Column("PRODUCT")]
+        public string MyString { get; set; }
+
+        [Column("UPDATE_TIME")]
+        public DateTime MyDate { get; set; }
+    }
+}
