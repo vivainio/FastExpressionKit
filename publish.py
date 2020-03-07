29a48b1f7c0cf0f8da28882f2afad964e25d07c2
Ville M. Vainio <ville.vainio@basware.com>
2020-03-07 22:05:08 +0200

    use local nuget

--- publish.py
+++ publish.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 import os,shutil
 
+LOCAL_FEED = Path(r".\dev\localnuget").absolute()
+
 projects = ["FastExpressionKit", "FastExpressionKit.BulkInsert"]
 version = "1.2.0.0"
 def c(s):
@@ -15,14 +17,19 @@ def nuke(pth):
     if os.path.isdir(pth):
         shutil.rmtree(pth)
 
+def nuget_add(pth):
+    c(f"nuget add -Verbosity detailed -Source {LOCAL_FEED} {pth}")
+
 startdir = Path(".").absolute()
 
-for prjdir in projects:
+for prjdir in projects:    
     os.chdir(startdir / prjdir)
     nuke("bin")
     nuke("obj")
 
     def pack():
         c(f"dotnet pack -c Release /p:Version={version} /p:PubVer={version}")
+        pkgs = list(Path("bin/Release").glob("*.nupkg"))
+        nuget_add(pkgs[0])
 
     pack()
