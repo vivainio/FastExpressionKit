347dca271a87f324e4b96c21a96d94534e78ab91
Ville M. Vainio <ville.vainio@basware.com>
2020-03-08 21:44:59 +0200

    bump version

--- publish.py
+++ publish.py
@@ -7,7 +7,7 @@ import os,shutil
 LOCAL_FEED = Path(r".\dev\localnuget").absolute()
 
 projects = ["FastExpressionKit", "FastExpressionKit.BulkInsert"]
-version = "1.2.0.4"
+version = "1.3.0"
 def c(s):
     print(">",s)
     err = os.system(s)
