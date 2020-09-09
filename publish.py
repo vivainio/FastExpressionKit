3af8d602f97f023335dd4277a3f5d3f60c065bde
Ville M. Vainio <ville.vainio@basware.com>
2020-09-09 19:07:00 +0300

    1.5.0

--- publish.py
+++ publish.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 from pathlib import Path
 
-import os,shutil
+import os, shutil
 
 LOCAL_FEED = Path(r".\dev\localnuget").absolute()
 
@@ -10,22 +10,27 @@ if not LOCAL_FEED.exists():
     os.makedirs(LOCAL_FEED)
 
 projects = ["FastExpressionKit", "FastExpressionKit.BulkInsert"]
-version = "1.4.0"
+version = "1.5.0"
+
+
 def c(s):
-    print(">",s)
+    print(">", s)
     err = os.system(s)
     assert not err
+
 
 def nuke(pth):
     if os.path.isdir(pth):
         shutil.rmtree(pth)
 
+
 def nuget_add(pth):
     c(f"dotnet nuget push -s {LOCAL_FEED} {pth}")
 
+
 startdir = Path(".").absolute()
 
-for prjdir in projects:    
+for prjdir in projects:
     os.chdir(startdir / prjdir)
     nuke("bin")
     nuke("obj")
